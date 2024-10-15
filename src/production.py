864077db54d90ba78fe9931110bf6729d7367a3c
devMotcho <code.mmd2@gmail.com>
2024-10-15 16:03:06 +0100

    csrf trusted origins

--- src/production.py
+++ src/production.py
@@ -11,4 +11,8 @@ DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+        os.environ.get('RAILWAY_STATIC_URL', ''),
+    ]
+
 CORS_ORIGIN_ALLOW_ALL = True
