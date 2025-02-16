50a6f395bba6a6ddfc28b9dcad9435b910d10b3d
devMotcho <code.mmd2@gmail.com>
2025-02-16 22:34:07 +0000

    Added IAM and S3 Bucket to store images

--- src/settings.py
+++ src/settings.py
@@ -126,4 +126,13 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AWS_QUERYSTRING_AUTH = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# AWS IAM and Bucket
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+
 paginator_num = 10
